b5c1241078b143fc1b67b5ce8935ac6091e17757
alexander171294 <alexander171294@live.com>
2015-12-16 12:42:00 -0300

    new user error handler

---
+++ phpquery/components/userErrorHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+class _e
+{
+	static protected $errors = array();
+	static protected $language = null;
+	
+	static public function set($errorKey, $errorMessage)
+	{
+		self::$errors[$errorKey] = array('code' => count(self::$errors), 'key' => $errorKey, 'message' => $errorMessage);
+	}
+	
+	static public function get($errorKey)
+	{
+		$errorMessage = self::getTranslated($errorKey);
+		// si no hay en idioma usamos el predefinido
+		if(empty($errorMessage)) $errorMessage = self::$errors[$errorKey]['message'];
+		return json_encode(array('error'=>true, 'code' => self::$errors[$errorKey]['code'], 'key' => $errorKey, 'message' => $errorMessage));
+	}
+	
+	static public function getTranslated($errorKey)
+	{
+		$out = null;
+		if(is_array(self::$language) && isset(self::$language[$errorKey]))
+			$out = self::$language[$errorKey];
+		return $out;
+	}
+	
+	public function loadLanguage($file)
+	{
+		self::$language = json_decode(file_get_contents($file));
+	}
+}
