06f5e30349c72662148eb2d2581a568c3ccf9997
alexander171294 <alexander171294@live.com>
2016-01-12 11:06:43 -0300

    added curlPost to curl component

--- phpquery/components/thirdparty/curl.php
+++ phpquery/components/thirdparty/curl.php
@@ -16,3 +16,23 @@ function curlGet($link, $timeOut = 3)
 	curl_close($curl);
 	return $result;
 }
+
+function curlPost($url, $fields = array())
+{
+	$fields_string = '';
+	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	rtrim($fields_string, '&');
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_POST, count($fields));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_REFERER, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_USERAGENT,'iPhone');
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
