010c4bab8d800db02b514ffa9d3c9ef77c543feb
alexander171294 <alexander171294@live.com>
2016-01-20 10:49:32 -0300

    Add check structure in loader.php

--- phpquery/loader.php
+++ phpquery/loader.php
@@ -7,7 +7,17 @@ if(version_compare(PHP_VERSION, '5.5.0', '<')) die('<b>FATAL ERROR</b> PHPQuery 
 // check permissions
 //...
 // validate core structure 
-//...
+try {
+	if(!file_exists(__DIR__.'/default_config/coreData.php')) throw new Exception('<b>FATAL ERROR</b> default_config/coreData.php is\'nt exists');
+	if(!file_exists(__DIR__.'/kernel.php')) throw new Exception('<b>FATAL ERROR</b> kernel.php is\'nt exists');
+	if(!file_exists(__DIR__.'/default_config/dbData.php')) throw new Exception('<b>FATAL ERROR</b> default_config/dbData.php is\'nt exists');
+	if(!file_exists(__DIR__.'/default_config/tplData.php')) throw new Exception('<b>FATAL ERROR</b> default_config/tplData.php is\'nt exists');
+	if(!file_exists(__DIR__.'/class.date.php')) throw new Exception('<b>FATAL ERROR</b> class.date.php is\'nt exists');
+	if(!file_exists(__DIR__.'/class.inputvars.php')) throw new Exception('<b>FATAL ERROR</b> class.inputvars.php is\'nt exists');
+} catch(Exception $e)
+{
+	die($e->getMessage());
+}
 
 // Load files //
 
